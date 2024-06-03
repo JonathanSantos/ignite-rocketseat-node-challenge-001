06aba625a2d4e77841e602379e911d8f9db35795
Jonathan <brjs.santos@gmail.com>
2024-06-03 18:56:33 -0300

    chore: feito um pequeno refactor e ajustado a regra de campos requiridos na criação da task

--- src/api.js
+++ src/api.js
@@ -23,16 +23,30 @@ taskController.get('/tasks', async (req, res) => {
     return true
   });
 
-  res.writeHead(200)
-  res.end(JSON.stringify(filteredTasks))
+  res
+    .writeHead(200)
+    .end(JSON.stringify(filteredTasks))
 });
 
 taskController.post('/tasks', async (req, res) => {
   const { title, description } = req.body ?? {}
+
+  if (!title) {
+    return res
+      .writeHead(400)
+      .end(JSON.stringify({ message: 'The "title" attribute is required in body request'}))
+  }
+
+  if (!description) {
+    return res
+      .writeHead(400)
+      .end(JSON.stringify({ message: 'The "description" attribute is required in body request'}))
+  }
+
   const task = { 
     id: randomUUID(),
-    title: title ?? '',
-    description: description ?? '',
+    title: title,
+    description: description,
     created_at: new Date(),
     updated_at: null,
     completed_at: null
@@ -40,8 +54,7 @@ taskController.post('/tasks', async (req, res) => {
 
   await database.insert(TABLE_TASKS, task)
 
-  res.writeHead(201)
-  res.end()
+  res.writeHead(201).end()
 });
 
 taskController.put('/tasks/:id', async (req, res) => {
@@ -55,25 +68,26 @@ taskController.put('/tasks/:id', async (req, res) => {
   } else if (description) {
     task.description = description
   } else {
-    res.writeHead(400)
-    res.end(JSON.stringify({ message: 'The "title" or "description" attribute is required in body request'}))
-    return
+    return res
+      .writeHead(400)
+      .end(JSON.stringify({ message: 'The "title" or "description" attribute is required in body request'}))
   }
 
   const hasTask = database.select(TABLE_TASKS).some(task => task.id === id)
 
   if (!hasTask) {
-    res.writeHead(404)
-    res.end(JSON.stringify({ message: 'Task not found'}))
-    return
+    return res
+      .writeHead(404)
+      .end(JSON.stringify({ message: 'Task not found'}))
   }
   
   task.updated_at = new Date()
 
   await database.update(TABLE_TASKS, id, task)
 
-  res.writeHead(200)
-  res.end(JSON.stringify({ message: 'Task updated'}))
+  res
+    .writeHead(200)
+    .end(JSON.stringify({ message: 'Task updated'}))
 });
 
 taskController.delete('/tasks/:id', async (req, res) => {
@@ -82,15 +96,14 @@ taskController.delete('/tasks/:id', async (req, res) => {
   const hasTask = database.select(TABLE_TASKS).some(task => task.id === id)
 
   if (!hasTask) {
-    res.writeHead(404)
-    res.end(JSON.stringify({ message: 'Task not found'}))
-    return
+    return res
+      .writeHead(404)
+      .end(JSON.stringify({ message: 'Task not found'}))
   }
 
   await database.delete(TABLE_TASKS, id)
 
-  res.writeHead(204)
-  res.end()
+  res.writeHead(204).end()
 });
 
 taskController.patch('/tasks/:id/complete', async (req, res) => {
@@ -99,19 +112,20 @@ taskController.patch('/tasks/:id/complete', async (req, res) => {
   const task = database.select(TABLE_TASKS).find(task => task.id === id)
 
   if (!task) {
-    res.writeHead(404)
-    res.end(JSON.stringify({ message: 'Task not found'}))
-    return
+    return res
+      .writeHead(404)
+      .end(JSON.stringify({ message: 'Task not found'}))
   }
 
   if (task.completed_at) {
-    res.writeHead(400)
-    res.end(JSON.stringify({ message: 'Task already completed'}))
-    return
+    return res
+      .writeHead(400)
+      .end(JSON.stringify({ message: 'Task already completed'}))
   }
 
   await database.update(TABLE_TASKS, id, { completed_at: new Date() })
 
-  res.writeHead(200)
-  res.end(JSON.stringify({ message: 'Task completed'}))
+  res
+    .writeHead(200)
+    .end(JSON.stringify({ message: 'Task completed'}))
 });
